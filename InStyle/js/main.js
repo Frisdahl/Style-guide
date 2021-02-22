b51031f1ec5ad25b1eb8ddc70dd6590dbf2e18b8
tjobz <worlt100@gmail.com>
2021-02-22 12:14:37 +0100

    Update App

--- InStyle/js/main.js
+++ InStyle/js/main.js
@@ -4,19 +4,53 @@ var loadInScreen = document.getElementById("load-in");
 var loginScreen = document.getElementById("login");
 var homeScreen = document.getElementById("home-screen");
 
+var headerLogo = document.getElementById("header-logo")
+
 
 // Load In
 setTimeout(function () {
-    loadInScreen.classList.remove("active-section")
-    loginScreen.classList.add("active-section")
+
+    loadInScreen.classList.add("fadeout");
+    
+
+    setTimeout(function () {
+        
+        headerLogo.classList.remove("invisible");
+
+        loadInScreen.classList.remove("active-section");
+        loginScreen.classList.add("active-section");
+        
+
+        setTimeout(function () {
+            
+            headerLogo.classList.add("fadein");
+            loginScreen.classList.add("fadein");
+            
+        }, 200)
+
+    }, 200)
+
 }, 3000) // time in millisecond for as long as you like
+
+
+function login() {
+    var username = document.getElementById("login-username");
+    console.log(username.value);
+}
+
+
+
+
+
+
+
 
 
 function goToHome() {
 
     console.log(this);
     var parentOfButton = this.parentElement.nodeName;
-    console.log(parentOfButton)
+    console.log(parentOfButton);
 
 }
 
